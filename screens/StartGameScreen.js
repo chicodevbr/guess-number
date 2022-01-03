2b05e7c10415b343e2bb2d8b420a3a987654f9dd
chicodevbr <chicodevbr@gmail.com>
2022-01-03 19:44:54 -0300

    add small sizes screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,6 +6,7 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
+  Dimensions,
 } from 'react-native';
 import BodyText from '../components/BodyText';
 import MainButton from '../components/Button';
@@ -115,8 +116,10 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   inputContainer: {
-    width: 300,
-    maxWidth: '80%',
+    width: '80%',
+    //maxWidth: '80%',
+    maxWidth: '95%',
+    minWidth: 300,
     alignItems: 'center',
   },
   buttonContainer: {
@@ -126,7 +129,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   button: {
-    width: 90,
+    width: Dimensions.get('window').width / 4,
   },
   input: {
     width: 50,
