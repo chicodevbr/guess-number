0478a7cc2486bdad50c7e1baaefcdc0ff2da3428
chicodevbr <chicodevbr@gmail.com>
2022-01-05 19:15:08 -0300

    add Platform.select

--- components/Header.js
+++ components/Header.js
@@ -5,23 +5,39 @@ import TitleText from './TitleText';
 
 const Header = (props) => {
   return (
-    <View style={styles.header}>
+    <View
+      style={{
+        ...styles.headerBase,
+        ...Platform.select({
+          ios: styles.headerIOS,
+          android: styles.headerAndroid,
+        }),
+      }}
+    >
       <TitleText style={styles.title}>{props.title}</TitleText>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: {
+  headerBase: {
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Platform.OS === 'android' ? Colors.primary : 'white',
     alignItems: 'center',
     justifyContent: 'center',
-    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
   },
+  headerIOS: {
+    backgroundColor: Colors.primary,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  headerAndroid: {
+    backgroundColor: 'white',
+    borderBottomColor: 'transparent',
+    borderBottomWidth: 0,
+  },
+
   title: {
     color: Platform.OS === 'ios' ? Colors.primary : 'white',
   },
