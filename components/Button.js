ff7dc47c93d51913f5a2d26e16ab5594e5e064e9
chicodevbr <chicodevbr@gmail.com>
2022-01-05 19:20:17 -0300

    created button component dinamically

--- components/Button.js
+++ components/Button.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Platform,
+  TouchableNativeFeedback,
+} from 'react-native';
 import Colors from '../constants/colors';
 
 const MainButton = (props) => {
+  let ButtonComponent = TouchableOpacity;
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    ButtonComponent = TouchableNativeFeedback;
+  }
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+    <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
       <View style={styles.button}>
         <Text style={styles.buttonText}>{props.children}</Text>
       </View>
-    </TouchableOpacity>
+    </ButtonComponent>
   );
 };
 
